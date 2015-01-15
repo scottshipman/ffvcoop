feed1191627924351433cc5c7e3b2e72a82bb1da
Scott Shipman <scottshipman@aol.com>
2015-01-15 07:24:04 -0500

    nice menus and calendar

--- sites/all/themes/ffvcoop/templates/page.tpl.php
+++ sites/all/themes/ffvcoop/templates/page.tpl.php
@@ -70,27 +70,30 @@
 
     <div id="navigation">
 
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-          		<div id="phone-nav">Phone (727) 488-0858</div>
-        </nav>
-      <?php endif; ?>
+        <?php if (module_exists('nice_menus')): ?>
+            <nav id="main-menu" role="navigation">
+                <?php
+                print theme('nice_menus_main_menu', array(
+                    'direction' => 'down',
+                    'depth' => -1,
+                )); ?>
+            </nav>
+        <?php elseif ($main_menu): ?>
+            <nav id="main-menu" role="navigation">
+                <?php
+                print theme('links__system_main_menu', array(
+                    'links' => $main_menu,
+                    'attributes' => array(
+                        'class' => array('links', 'inline', 'clearfix'),
+                    ),
+                    'heading' => array(
+                        'text' => t('Main menu'),
+                        'level' => 'h2',
+                        'class' => array('element-invisible'),
+                    ),
+                )); ?>
+            </nav>
+        <?php endif; ?>
 
 
       <?php print render($page['navigation']); ?>
